29c82e7ecf006ffbd12b16588f1535eb94ad9c27
Jacob Hoogstra <jnhoogstra@crimson.ua.edu>
2021-04-06 13:15:10 -0500

    added department and employee cs files with some fields

---
+++ MIS421_FinalProject/Models/Department.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Permissions;
+using System.Threading.Tasks;
+
+namespace MIS421_FinalProject.Models
+{
+    public class Department
+    {
+        [Key]
+        public Guid deptID { get; set; }
+
+        [Required]
+        public string deptName { get; set; }
+        [Required]
+        public string deptAddress { get; set; }
+        [Required]
+        public string deptCity { get; set; }
+        [Required]
+        public string deptCountry { get; set; }
+
+        
+        public string deptState { get; set; }
+        public string deptZip { get; set; }
+    }
+}
---
+++ MIS421_FinalProject/Models/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Permissions;
+using System.Threading.Tasks;
+
+namespace MIS421_FinalProject.Models
+{
+    public class Employee
+    {
+        [Key]
+        public Guid empID { get; set; }
+
+        [Required]
+        public string empFName { get; set; }
+
+        [Required]
+        public string empLName { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime empBDate { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime empHireDate { get; set; }
+        public  string empPhone { get; set; }
+        public string empAddress { get; set; }
+        public string empCity { get; set; }
+        public string empZip { get; set; }
+
+
+        //Foreign key to Department
+        public virtual Department Department { get; set; }
+    }
+}
