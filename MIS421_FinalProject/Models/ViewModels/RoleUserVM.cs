931e5edea1c46bffa212e92ee0387616253e6482
bgnordberg <bgnordberg@crimson.ua.edu>
2021-04-27 02:13:52 -0500

    Added Users View, Authentication to create user and assign user roles

---
+++ MIS421_FinalProject/Models/ViewModels/RoleUserVM.cs
@@ -0,0 +1,15 @@
+﻿using MIS421_FinalProject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MIS421_FinalProject
+{
+    public class RoleUserVM
+    {
+        public ApplicationUser AppUser { get; set; }
+        public List<string> UserRoles { get; set; }
+        public List<string> AllRoles { get; set; }
+    }
+}
